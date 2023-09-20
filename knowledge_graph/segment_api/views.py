7ec9a653cf510f7c857df682bf3f7824925717f8
hal468a <asdfg880226@gmail.com>
2023-09-20 14:04:12 +0800

    upadet views.py

--- knowledge_graph/segment_api/views.py
+++ knowledge_graph/segment_api/views.py
@@ -5,82 +5,36 @@ from translate import Translator
 import json, re, jieba, time
 from opencc import OpenCC
 
-# 移除文字中的網址
 def removeURL(text):
+    # 刪除URL網址
     url_pattern = re.compile(r'https?://\S+|www\.\S+')
     return url_pattern.sub(r'', text)
 
-# 簡體轉繁體
 def simplifiedToTraditionalChineseOpenCC(text):
+    # 將簡體中文轉成繁體中文
     converter = OpenCC('s2twp')
     return converter.convert(text)
 
-def chineseQuoteReplaceRE(text):
-
-    textList = list(text)
-
-    flag = 0
-    for i in range(len(textList)):
-        temp = textList[i]
-        if temp == '『':
-            if flag == 0 and temp == '『':
-                continue
-            flag = 1
-        elif temp == '』':
-            if flag == 1 and temp == '』':
-                continue
-            flag = 0 
-        
-        if flag == 0:
-            continue
-        elif flag == 1:
-            if temp == "。":
-                textList[i] = u"\uFF0C"
-
-    text = ''.join(textList)
-    return text
-
-# 英文轉繁體
-def English2ChineseTranslation(text):
-    pattern = r'[A-Za-z]+'
-    english_parts = re.findall(pattern, text)
-    for part in english_parts:
-        traditional_chinese_part = Translator(from_lang='en', to_lang='zh-TW').translate(part)
-        text = text.replace(part, traditional_chinese_part)
-    return text
-
 def removeCustomCharactersRE(text):
-    text = re.sub('[\u0040-\u007E\uFF20-\uFF60]', r'', text)
-    text = re.sub('~#$%&()<=>`\uFF5E\uFF03-\uFF06\uFF08\uFF09\uFF1C-\uFF1E', r'', text) 
+    # 刪除英文字母與全形英文字母
+    text = re.sub('[\u0040-\u007E\uFF20-\uFF60]', r'', text) 
+    # 刪除符號與全形符號
+    text = re.sub('[\uFF5E\uFF03-\uFF06\uFF08\uFF09\uFF1C-\uFF1E]', r'', text) 
+    # 刪除數字與全形數字
+    text = re.sub('[\u0030-\u0039\uFF10-\uFF19]', r'', text) 
+    # 刪除特定符號
+    text = re.sub('[~#$%&()<=>\"]', r'', text)
     return text
-
-# 停用詞
-def stopWord(text):
-    # stopwords = {}.fromkeys([ line.rstrip() for line in open('stopWords.txt', encoding='utf8') ])
-    stopwords = [word.strip('\n') for word in open('customStopWords.txt', encoding='utf8')]
-    # i = 0
-    testList = []
-    for line in text:
-        # if i == 1:
-        #     break;
-        # print(line)
-        segmentation = jieba.cut(line, cut_all=False)
-        str = ''
-        for s in segmentation:
-            if s not in stopwords:
-                str += s
-        # i += 1
-        testList.append(str)
-    return testList
 
 def cutSentenceRE(text):
-    # for 中文全形
-    text = re.sub('([.。！？\?])([^’”\"\'])',r'\1\n\2', text)
-    text = re.sub('(\.{6})([^’”\"\'])',r'\1\n\2', text)
-    text = re.sub('(\…{2})([^’”\"\'])',r'\1\n\2', text)
-    # text = re.sub('([.。！？\?\.{6}\…{2}][’”])([^’”])',r'\1\n\2', text) 
-    # text = re.sub('~',r'', text)
+    # 為了中文全形的分句，根據句號驚嘆號問號做換行
+    text = re.sub('([.。！？\?])([^’”\"\'])',r'\1\n\2', text) 
+    # 為了中文全形的分句，根據全形刪節號做換行
+    text = re.sub('(\.{6})([^’”\"\'])',r'\1\n\2', text) 
+    # 為了中文半形的分句，根據半形刪節號做換行
+    text = re.sub('(\…{2})([^’”\"\'])',r'\1\n\2', text) 
 
+    # 删除 string 字串末尾的指定符號並做分句
     text = text.rstrip()
     return text.split("\n")
 
@@ -93,7 +47,6 @@ def process_text(request:WSGIRequest):
             # print(text['text'], type(text['text']))
 
             text = simplifiedToTraditionalChineseOpenCC(text["text"])
-            # text = English2ChineseTranslation(text)
             text = removeURL(text)
             text = removeCustomCharactersRE(text)
             text = cutSentenceRE(text)
